2bc11c760294ce85a3653c17a3aae0ac5516f489
igoroshust <igoroshust75@gmail.com>
2025-07-21 10:12:04 +0900

    feat: modif updateTimer

---
+++ frontend/public/js/updateTimer.js
@@ -0,0 +1,13 @@
+    function updateTimer() {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+
+        document.getElementById('timer').textContent = `${hours}:${minutes}:${seconds}`;
+    }
+
+    // Обновляем таймер каждую секунду
+    setInterval(updateTimer, 1000);
+
+updateTimer();
